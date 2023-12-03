bc8ca1f60476870e84c87061381fc193a0cea791
Kamilcz <kamilcz@o2.pl>
2023-12-03 19:01:41 +0100

    dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿Console.WriteLine("Cześć Kamil!");
+﻿var imię = "Ewa";
+var płeć = "Kobieta";
+var wiek1 = 33;
+var wiek2 = 30;
+var wiek3 = 18;
 
+
+if (imię == "Ewa" && wiek1 == 33)
+{
+    Console.WriteLine("Ewa, 33 lata");
+}
+else if (płeć == Kobieta && wiek1 < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (płeć != kobieta && wiek3 < 18)
+{
+    Console.WriteLine("Mężczyzna niepełnoletni");
+}
